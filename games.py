4acf5f61e60def161e00d758241f97539252c880
Hachibkhan <hachib.khan19@gmail.com>
2019-05-09 16:04:14 +0600

    first commit

---
+++ games.py
@@ -0,0 +1,41 @@
+import random
+
+number = random.randint(1, 1000)
+
+attempts = 0
+
+low = 1
+
+high = 1000
+
+while True:
+
+    print("Guess the number (between 1 and 1000):")
+
+    input_number = (low + high) // 2 # only integer division
+
+    print("My guess is", input_number)
+
+    attempts += 1
+
+    if input_number == number:
+
+        print("Yes, your guess is correct!")
+
+        break
+
+    if input_number > number:
+
+        print("Incorrect! please try to guess a smaller number.")
+
+        high = input_number - 1
+
+    else:
+
+        print("Incorrect! please try to guess a large number.")
+
+        low = input_number - 1 
+
+print("You tried", attempts, "times to find the correct number.")
+
+
---
+++ prime_number.py
@@ -0,0 +1,29 @@
+import math
+
+def is_prime(n):
+
+    if n < 2:
+
+        return False
+
+    if n == 2:
+
+        return True
+
+    if n == 0:
+
+        return False
+
+    m = math.sqrt(n)
+
+    m = int(m) + 1
+
+    for x in range(3, m, 2):
+
+        if n % x == 0:
+
+            return False
+
+        return True
+
+
